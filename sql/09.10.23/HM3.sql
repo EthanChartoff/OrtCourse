d841986b03af28f502fc2d96fce488c1479e737f
EthanChartoff <Chartoff.ethan@gmail.com>
2023-09-11 20:30:41 +0300

    sql HM3 done

--- sql/09.10.23/HM3.sql
+++ sql/09.10.23/HM3.sql
@@ -8,5 +8,22 @@ select min(lastName) from employees;
 select sum(Quantity*UnitPrice*(1-Discount)) from orderdetails where OrderId=10248;
 */
 
-/* Problem 3 */
-select OrderID from orderdetails where UnitPrice=2.000;
+/* Problem 3 
+select OrderID from orderdetails where UnitPrice=(select min(UnitPrice) from orderdetails);
+*/
+
+/* Problem 4 
+select avg(UnitPrice) from orderdetails;
+*/
+
+/* Problem 5 
+select avg(QuantityPerUnit) from products;
+*/
+
+/* Problem 6 
+select count(*) from customers where (City='London' or City='Berlin');
+*/
+
+/* Problem 7 
+select min(Salary) as 'smallest salary', max(Salary) as 'largest salary', avg(Salary) as 'average salary' from employees;
+*/
